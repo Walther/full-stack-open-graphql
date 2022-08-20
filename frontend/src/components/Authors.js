c39833135247cd31e3a7506f0c5b0884d50a4e81
Walther <veeti.haapsamo@gmail.com>
2022-08-20 16:43:44 +0300

    Solve exercise 8.11 - 8.12

--- frontend/src/components/Authors.js
+++ frontend/src/components/Authors.js
@@ -1,4 +1,34 @@
+import { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
+
+const EDIT_AUTHOR_BIRTH = gql`
+  mutation editAuthorBirth($name: String!, $year: Int!) {
+    editAuthor(name: $name, setBornTo: $year) {
+      id
+      name
+      born
+    }
+  }
+`;
+
 const Authors = (props) => {
+  const [selected, setSelected] = useState("");
+  const [year, setYear] = useState("");
+  const [editAuthorBirth] = useMutation(EDIT_AUTHOR_BIRTH);
+
+  const submit = async (event) => {
+    event.preventDefault();
+    editAuthorBirth({
+      variables: {
+        name: selected,
+        year: parseInt(year),
+      },
+    });
+
+    setYear("");
+    setSelected("");
+  };
+
   if (!props.show) {
     return null;
   }
@@ -22,6 +52,28 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+      <h2>set birthyear</h2>
+      <form onSubmit={submit}>
+        <select
+          value={selected}
+          onChange={({ target }) => setSelected(target.value)}
+        >
+          {props.authors.map((a) => (
+            <option value={a.name} key={a.id}>
+              {a.name}
+            </option>
+          ))}
+        </select>
+        <div>
+          born
+          <input
+            type="number"
+            value={year}
+            onChange={({ target }) => setYear(target.value)}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   );
 };
